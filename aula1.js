0e8a075cce66a1fe8801b82f47123c67d6ceea10
Ern-2022 <erniciojermiaschilund@gmail.com>
2024-03-18 23:34:37 +0200

    minha primeira vez a mergulhar no javascript

---
+++ aula1.js
@@ -0,0 +1,5 @@
+// g - global (Encontra todas as ocorrencias)]
+// i- insensitive
+
+const {texto} = require('./base')
+const regExp1 = /joao/g;
---
+++ base.js
@@ -0,0 +1,13 @@
+const texto = `O Joao trouxe flores para a sua amada namorada em 10 de janeiro de 1970,
+Maria era o nome dela.
+
+
+Foi um ano excelente na vida de Joao, Teve 5 filhos, todos adultos actualmente.
+Maria, hoje sua esposa ainda faz aquele cafe com pao de queijo nas tardes de domingo.
+Tambem ne! sendo a oa maneira que, nunca esquece seu famoso pao de queijo.
+Nao canso de ouvir a Maria:
+"Joooooooooaooooooo, o cafe ta protinho aqui, veeemm."`;
+
+module.exports = {
+    texto
+}
